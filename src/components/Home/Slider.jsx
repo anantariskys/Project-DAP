aadf1f9bb08260c376d3f8be7cbd6617c1842033
anantariskys <gamersindo555@gmail.com>
2023-11-25 13:00:20 +0700

    bismillah testing roadmap

--- src/components/Home/Slider.jsx
+++ src/components/Home/Slider.jsx
@@ -19,6 +19,7 @@ import right from "../../assets/roadmap/Right-button.svg";
 import Aos from 'aos';
 import 'aos/dist/aos.css';
 import React, { useEffect } from "react";
+import { Link } from 'react-router-dom';
 
 
 
@@ -130,12 +131,12 @@ const Slider = () => {
               <img src={fe} className="mx-auto mb-3 h-24 " alt="" />
               <h1 className="text-white text-3xl font-Poppins font-extrabold mb-2">Frontend</h1>
               <p className="text-xl text-white font-Poppins h-20 leading-7 mb-9">Melangkah Menjadi Frontend terdepan</p>
-              <a href="/">
+              <Link to="/roadMap">
                 <div className="w-28 p-2 gap-3 mb-14 border-[1px]  bg-slate-400 bg-opacity-20 rounded-3xl flex justify-center flex-row text-white font-Poppins align-middle">
                   <p>Eksplor</p>
                   <img src={linkcard} alt="" />
                 </div>
-              </a>
+                </Link>
             </div>
           </SwiperSlide>
           <SwiperSlide>
